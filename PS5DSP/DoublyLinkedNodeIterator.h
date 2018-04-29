25d4f6bab53dbf779a1d59b9096c3c89583f487c
Dylski <dylski@dylski>
2018-04-29 17:40:31 +1000

    Final

--- PS5DSP/DoublyLinkedNodeIterator.h
+++ PS5DSP/DoublyLinkedNodeIterator.h
@@ -28,62 +28,99 @@ public:
 template<class DataType>
 DoublyLinkedNodeIterator<DataType>::DoublyLinkedNodeIterator(const Node& aList)
 {
-
+	fCurrent = &aList;
+	while (&fCurrent->getNext() != &Node::NIL)
+	{
+		fCurrent = &fCUrrent->getNext();
+		fRightmost = fCurrent;
+		while (&fCUrrent->getPrevious() != &Node::NIL)
+		{
+			fCurrent = &fCurrent->getPrevious();
+		}
+		fLeftmost = fCurrent;
+	}
 }
 template<class DataType>
 const DataType& DoublyLinkedNodeIterator<DataType>:: operator*() const
 {
-
+	return fCurrent->getValue();
 }
 template<class DataType>
 Iterator& DoublyLinkedNodeIterator<DataType>::operator++()
 {
-
+	if (fState = BEFORE)
+	{
+		fState = DATA;
+		fCurrent = fLeftmost;
+	}
+	return *this;
 }
 template<class DataType>
 Iterator& DoublyLinkedNodeIterator<DataType>::operator++(int)
 {
-
+	DoublyLinkedNodeIterator temp = *this;
+	++(*this);
+	return temp;
 }
 template<class DataType>
 Iterator& DoublyLinkedNodeIterator<DataType>::operator--()
 {
-
+	if (fState = AFTER)
+	{
+		fState = DATA;
+		FCurrent = fRightmost;
+	}
+	return *this;
 }
 template<class DataType>
 Iterator DoublyLinkedNodeIterator<DataType>::operator--(int)
 {
-
+	DoublyLinkedNodeIterator temp = *this;
+	--(*this);
+	return temp;
 }
 template<class DataType>
 bool DoublyLinkedNodeIterator<DataType>::operator==(const Iterator& aOtherIter) const
 {
-
+	return(fCurrent == aOtherIter.fCurrent &&
+		fLeftmost == aOtherIter.fLeftmost &&
+		fRightmost == aOtherIter.fRightmost &&
+		fState == aOtherIter.fState);
 }
 template<class DataType>
 bool DoublyLinkedNodeIterator<DataType>::operator!=(const Iterator& aOtherIter) const
 {
-
+	return (*this == aOtherIter);
 }
 template<class DataType>
-Iterator  DoublyLinkedNodeIterator<DataType>::begin() const
+Iterator DoublyLinkedNodeIterator<DataType>::begin() const
 {
-
+	DoublyLinkedNodeIterator temp = *this;
+	temp.fState = DATA;
+	temp.fCUrrent = fLeftmost;
+	return temp;
 }
 template<class DataType>
-Iterator  DoublyLinkedNodeIterator<DataType>::end() const
+Iterator DoublyLinkedNodeIterator<DataType>::end() const
 {
-
+	DoublyLinkedNodeIterator temp = *this;
+	temp.fState = AFTER;
+	temp.fCurrent = &node::NIL;
 }
 template<class DataType>
-Iterator  DoublyLinkedNodeIterator<DataType>::rbegin() const
+Iterator DoublyLinkedNodeIterator<DataType>::rbegin() const
 {
-
+	DoublyLinkedNodeIterator temp = *this;
+	temp.fState = DATA;
+	temp.FCUrrent = fLeftmost;
+	return temp;
 }
 template<class DataType>
-Iterator  DoublyLinkedNodeIterator<DataType>::rend() const
+Iterator DoublyLinkedNodeIterator<DataType>::rend() const
 {
-
+	DoublyLinkedNodeIterator temp = *this;
+	temp.fState = BEFORE;
+	temp.fCurrent == &Node::NIL;
 }
 
 
